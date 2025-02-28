8d4a3c8332098a958affaa5daf9fa372d493577b
Alaina Iris <alainairis@proton.me>
2025-02-28 04:09:49 -0500

    Wavelength display

--- src/components/AudioDisplay.js
+++ src/components/AudioDisplay.js
@@ -1,7 +1,31 @@
-export default function AudioDisplay() {
+import {Line} from 'react-chartjs-2';
+import { CategoryScale, Chart, LinearScale, PointElement, LineElement, Title, Tooltip, Legend} from "chart.js";
+export default function AudioDisplay({audioData}) {
+	console.log(audioData);
+	Chart.register(
+		CategoryScale,
+		LinearScale,
+		PointElement,
+		LineElement,
+		Title,
+		Tooltip,
+		Legend
+	);
 	return (
 		<div id="audio-display">
-		
+		<Line
+		data={audioData}
+		options={{
+			plugins: {
+				title: {
+					display: true,
+						text: "Percieved Frequency"
+				},
+					legend: {
+						display: false
+					}
+			}
+		}}/>
 		</div>
 	)
 }
